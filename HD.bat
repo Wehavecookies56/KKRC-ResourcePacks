a3ea6901a4e7298fee5c75cc4e8a6de6d57844bd
Abelatox <abel54225@gmail.com>
2021-01-15 00:17:24 +0100

    Update HD.bat

--- HD.bat
+++ HD.bat
@@ -1,2 +1,2 @@
 cd ".\KK Re Coded HD -ReMIX-"
-"C:\Program Files\WinRAR\WinRAR.exe" a -afzip "..\Releases\KK Re Coded HD -ReMIX-.zip" ".\assets" ".\pack.mcmeta" ".\pack.png"
+"C:\Program Files\WinRAR\WinRAR.exe" a -afzip "..\Releases\KK2 HDRP 1.15.2.zip" ".\assets" ".\pack.mcmeta" ".\pack.png"
