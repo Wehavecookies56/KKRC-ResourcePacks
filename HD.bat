52fd984a27cdec95547f1b75f288b1823ec6dbbb
Abelatox <abel54225@gmail.com>
2021-02-06 02:43:57 +0100

    Updated armors

--- HD.bat
+++ HD.bat
@@ -1,2 +1,2 @@
 cd ".\KK Re Coded HD -ReMIX-"
-"C:\Program Files\WinRAR\WinRAR.exe" a -afzip "..\Releases\KK2 HDRP 1.15.2.zip" ".\assets" ".\pack.mcmeta" ".\pack.png"
+"C:\Program Files\WinRAR\WinRAR.exe" a -afzip "..\Releases\KK2 HDRP 1.16.5.zip" ".\assets" ".\pack.mcmeta" ".\pack.png"
