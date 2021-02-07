2ff0ef2e33acec510f5ca90b2610324e0ed3b498
Abelatox <abel54225@gmail.com>
2021-02-07 12:20:39 +0100

    Rename

--- HDx128.bat
+++ HDx128.bat
@@ -1,2 +1,2 @@
-cd ".\KK Re Coded HD -ReMIX- x64"
+cd ".\KK Re Coded HD -ReMIX- x128"
 "C:\Program Files\WinRAR\WinRAR.exe" a -afzip "..\Releases\KK2 HDRP 1.16.5 x128.zip" ".\assets" ".\pack.mcmeta" ".\pack.png"
