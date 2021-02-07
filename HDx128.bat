b2cb33f4cbcb2acde95530a541eeaeda877205a9
Abelatox <abel54225@gmail.com>
2021-02-07 12:11:16 +0100

    x128 pt.2

---
+++ HDx128.bat
@@ -0,0 +1,2 @@
+cd ".\KK Re Coded HD -ReMIX- x64"
+"C:\Program Files\WinRAR\WinRAR.exe" a -afzip "..\Releases\KK2 HDRP 1.16.5 x128.zip" ".\assets" ".\pack.mcmeta" ".\pack.png"
--- HDx64.bat
+++
@@ -1,2 +0,0 @@
-cd ".\KK Re Coded HD -ReMIX- x64"
-"C:\Program Files\WinRAR\WinRAR.exe" a -afzip "..\Releases\KK2 HDRP 1.16.5 x64.zip" ".\assets" ".\pack.mcmeta" ".\pack.png"
