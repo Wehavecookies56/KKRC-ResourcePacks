5ac1a5a238b642ca5284ed2cbe9c92cda8fafb43
Abelatox <abel54225@gmail.com>
2021-12-06 23:43:03 +0100

    Updated script to use 7zip and added kh3 ultima weapon

--- HD.bat
+++ HD.bat
@@ -1,2 +1,2 @@
 cd ".\KK Re Coded HD -ReMIX-"
-"C:\Program Files\WinRAR\WinRAR.exe" a -afzip "..\Releases\KK2 HDRP 1.16.5.zip" ".\assets" ".\pack.mcmeta" ".\pack.png"
+"C:\Program Files\7-Zip\7z.exe" a "..\Releases\KK2 HDRP 1.16.5.zip" ".\assets" ".\pack.mcmeta" ".\pack.png"
